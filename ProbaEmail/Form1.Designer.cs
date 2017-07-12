b3e2fdc47cac9e56cdc11f51a06adb7c42aa5275
szkrecsi <ecsenszki.judit@gmail.com>
2017-07-12 22:06:55 +0200

    getMessages method is added

--- ProbaEmail/Form1.Designer.cs
+++ ProbaEmail/Form1.Designer.cs
@@ -29,36 +29,128 @@
         private void InitializeComponent()
         {
             this.btnNewMessageForm = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtSearch = new System.Windows.Forms.TextBox();
+            this.picSearch = new System.Windows.Forms.PictureBox();
+            this.dataMessages = new System.Windows.Forms.DataGridView();
+            this.From = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Subject = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Message = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Date = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.picSearch)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataMessages)).BeginInit();
             this.SuspendLayout();
             // 
             // btnNewMessageForm
             // 
-            this.btnNewMessageForm.BackColor = System.Drawing.Color.Tomato;
-            this.btnNewMessageForm.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.btnNewMessageForm.ForeColor = System.Drawing.Color.Bisque;
-            this.btnNewMessageForm.Location = new System.Drawing.Point(12, 12);
+            this.btnNewMessageForm.BackColor = System.Drawing.Color.LightGray;
+            this.btnNewMessageForm.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.btnNewMessageForm.ForeColor = System.Drawing.Color.Red;
+            this.btnNewMessageForm.Location = new System.Drawing.Point(18, 52);
             this.btnNewMessageForm.Name = "btnNewMessageForm";
-            this.btnNewMessageForm.Size = new System.Drawing.Size(80, 33);
+            this.btnNewMessageForm.Size = new System.Drawing.Size(110, 36);
             this.btnNewMessageForm.TabIndex = 0;
             this.btnNewMessageForm.Text = "Compose";
             this.btnNewMessageForm.UseVisualStyleBackColor = false;
             this.btnNewMessageForm.Click += new System.EventHandler(this.button1_Click);
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("MS Reference Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.ForeColor = System.Drawing.Color.Blue;
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(126, 26);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Email App";
+            // 
+            // txtSearch
+            // 
+            this.txtSearch.BackColor = System.Drawing.SystemColors.Window;
+            this.txtSearch.Location = new System.Drawing.Point(188, 19);
+            this.txtSearch.Name = "txtSearch";
+            this.txtSearch.Size = new System.Drawing.Size(434, 20);
+            this.txtSearch.TabIndex = 2;
+            // 
+            // picSearch
+            // 
+            this.picSearch.BackColor = System.Drawing.Color.CornflowerBlue;
+            this.picSearch.Image = global::EmailApp.Properties.Resources.search;
+            this.picSearch.Location = new System.Drawing.Point(628, 20);
+            this.picSearch.Name = "picSearch";
+            this.picSearch.Size = new System.Drawing.Size(24, 19);
+            this.picSearch.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.picSearch.TabIndex = 3;
+            this.picSearch.TabStop = false;
+            this.picSearch.Click += new System.EventHandler(this.picSearch_Click);
+            // 
+            // dataMessages
+            // 
+            this.dataMessages.BackgroundColor = System.Drawing.SystemColors.Control;
+            this.dataMessages.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataMessages.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.From,
+            this.Subject,
+            this.Message,
+            this.Date});
+            this.dataMessages.Location = new System.Drawing.Point(146, 52);
+            this.dataMessages.Name = "dataMessages";
+            this.dataMessages.Size = new System.Drawing.Size(643, 339);
+            this.dataMessages.TabIndex = 4;
+            // 
+            // From
+            // 
+            this.From.HeaderText = "From";
+            this.From.Name = "From";
+            // 
+            // Subject
+            // 
+            this.Subject.HeaderText = "Subject";
+            this.Subject.Name = "Subject";
+            // 
+            // Message
+            // 
+            this.Message.HeaderText = "Message";
+            this.Message.Name = "Message";
+            this.Message.Width = 300;
+            // 
+            // Date
+            // 
+            this.Date.HeaderText = "Date";
+            this.Date.Name = "Date";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(493, 448);
+            this.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.ClientSize = new System.Drawing.Size(812, 494);
+            this.Controls.Add(this.dataMessages);
+            this.Controls.Add(this.picSearch);
+            this.Controls.Add(this.txtSearch);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.btnNewMessageForm);
             this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.picSearch)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataMessages)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.Button btnNewMessageForm;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtSearch;
+        private System.Windows.Forms.PictureBox picSearch;
+        private System.Windows.Forms.DataGridView dataMessages;
+        private System.Windows.Forms.DataGridViewTextBoxColumn From;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Subject;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Message;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Date;
     }
 }
 
--- ProbaEmail/Form1.cs
+++ ProbaEmail/Form1.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Windows.Forms;
-using System.Net.Mail;
-using System.Net;
+using AE.Net.Mail;
 
 namespace EmailApp
 {
@@ -10,12 +9,44 @@ namespace EmailApp
         public Form1()
         {
             InitializeComponent();
+            getMessages();
+        }
+
+        private void getMessages() {
+
+            string userName = "ycarecoder@gmail.com";
+            string password = "codecool";
+            ImapClient iclient = new ImapClient("imap.gmail.com", userName, password, AuthMethods.Login, 993, true);
+
+            iclient.SelectMailbox("inbox");
+            MessageBox.Show(iclient.GetMessageCount().ToString());
+
+            MailMessage[] messages = iclient.GetMessages(0, 9, false);
+            int i = 0;
+            string fileName = @"emailfile.dat";
+            foreach (MailMessage message in messages)
+            {
+                dataMessages.Rows.Add("");
+                dataMessages.Rows[i].Cells[0].Value = message.From;
+                dataMessages.Rows[i].Cells[1].Value = message.Subject;
+                dataMessages.Rows[i].Cells[2].Value = message.Body;
+                dataMessages.Rows[i].Cells[3].Value = message.Date.ToShortDateString();
+                
+                EmailClass email = new EmailClass(message.From.ToString(), message.Subject.ToString(), message.Body.ToString(), message.Date.ToShortDateString());
+                email.Serialize(fileName);
+                i++;
+            }
+            iclient.Dispose();
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
             Form2 formNewMessage = new Form2();
             formNewMessage.Show();
+        }
+
+        private void picSearch_Click(object sender, EventArgs e)
+        {
         }
     }
 }
